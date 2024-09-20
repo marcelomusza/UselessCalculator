85c46acc10733cf6422bbbbbfb85009b51452f25
Marcelo Muszalski <marcelo.muszalski@dcsl.com>
2024-09-20 14:02:25 +0200

    Basic Console Application with Dockerfile ready

---
+++ README.md
@@ -0,0 +1,11 @@
+# Console Application
+
+This is a simple .NET Console Application that performs basic arithmetic operations (addition, subtraction, multiplication, and division).
+
+The main idea of this project is to start from a very simple project, and continuously add complexity, following best coding practices, SOLID, design patterns and whatever is needed to make the project as complex and professional as possible.
+
+## Upgrades for this useless application
+- ~~Basic Console Application~~
+- ~~Add Dockerfile~~
+- Upgraded Console Application (Business Logic extracted)
+- Add WebAPI Project and connect it to the existing business logic
--- UselessCalculator.Console/Program.cs
+++ UselessCalculator.Console/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var num1 = GetValidNumber("Enter the first number: ");
+        var num2 = GetValidNumber("Enter the second number: ");
+
+        var selection = GetValidSelection();
+        
+        double result = selection switch
+        {
+            1 => num1 + num2,
+            2 => num1 - num2,
+            3 => num1 * num2,
+            4 => num1 / num2,
+            _ => throw new InvalidOperationException("Invalid operation.")
+        };
+
+        Console.WriteLine($"The result is: {result}");
+    }
+
+    static double GetValidNumber(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                var input = Console.ReadLine();
+
+                if (input is not null && double.TryParse(input, out var number))
+                {
+                    return number;
+                }
+
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+    static int GetValidSelection()
+    {
+        while (true)
+        {
+            Console.WriteLine("""
+            Enter the desired operation:
+            1 --> Sum
+            2 --> Subtract
+            3 --> Multiply
+            4 --> Divide
+            """);
+
+            var input = Console.ReadLine();
+
+            if (input is not null && int.TryParse(input, out var selection))
+            {
+                switch (selection)
+                {
+                    case >= 1 and <= 4:
+                        return selection;
+                }
+            }
+
+            Console.WriteLine("Invalid selection. Please enter a number between 1 and 4.");
+        }
+    }
+}
